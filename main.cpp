7e0abd5c8efebf8fd4b30566a9a1860dcaaedddb
wsandy <shengwangandy@foxmail.com>
2023-12-16 12:10:48 +0800

    add ray

--- main.cpp
+++ main.cpp
@@ -1,22 +1,59 @@
 #include <iostream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
 using GType=float;
 
+color<GType> ray_color(const ray<GType>& r){
+    vec3<GType> unit_direction = unit_vector(r.direction());
+    // 归一化向量，y轴-1~1, 映射到a的0~1
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    // 在白色到(0.5, 0.7, 1.0) 之间线性渐变
+    return (GType)(1.0-a) * color<GType>(1.0, 1.0, 1.0) + (GType)a * color<GType>(0.5, 0.7, 1.0);
+}
+
 int main(){
     // Image 
-    int image_width = 512;
-    int image_height = 512;
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
 
-    std::cout << "P3\n" << image_width << " " << image_width << std::endl << "255" << std::endl;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<GType>(image_width) / image_height);
+    auto camera_center = point3<GType>(0,0,0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3<GType>(viewport_width, 0, 0);
+    auto viewport_v = vec3<GType>(0, -viewport_height, 0);
+
+    //Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / (GType)image_width;
+    auto pixel_delta_v = viewport_v / (GType)image_height;
+
+    //Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center - vec3<GType>(0,0,focal_length) - viewport_u/(GType)2.0-viewport_v/(GType)2.0;
+    auto pixel00_loc = viewport_upper_left + (GType)0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+    std::cout << "P3\n" << image_width << " " << image_height << std::endl << "255" << std::endl;
 
     for(int j = 0; j < image_height; ++j){
 
         std::clog << "\rScanlines remaining: " << (image_height-j) << " " << std::flush;
 
         for(int i = 0; i < image_width; ++i){
-            auto pixel_color = color<GType>( GType(i)/(image_width-1), GType(j)/(image_height-1), 0 );
+            
+            auto pixel_center = pixel00_loc + ((GType)i * pixel_delta_u) + ((GType)j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray<GType> r(camera_center, ray_direction);
+            color<GType> pixel_color = ray_color(r);
+
             write_color(std::cout, pixel_color);
         }
     }
--- vec3.h
+++ vec3.h
@@ -60,22 +60,22 @@ inline std::ostream& operator<<(std::ostream &out, const vec3<T> &v){
 
 template <typename T>
 inline vec3<T> operator+(const vec3<T> &u, const vec3<T> &v){
-    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+    return vec3<T>(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
 template <typename T>
 inline vec3<T> operator-(const vec3<T> &u, const vec3<T> &v) {
-    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+    return vec3<T>(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 template <typename T>
 inline vec3<T> operator*(const vec3<T> &u, const vec3<T> &v) {
-    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+    return vec3<T>(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 template <typename T>
 inline vec3<T> operator*(T t, const vec3<T> &v) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+    return vec3<T>(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 template <typename T>
