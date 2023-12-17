5259628fcf1a341c1b94d2d6db8438b3e01cacc3
sheng <shengwangandy@foxmail.com>
2023-12-17 12:47:15 +0800

    加上gamma校正

--- src/color.h
+++ src/color.h
@@ -7,6 +7,14 @@
 template<typename  T>
 using color = vec3<T>;
 
+
+template<typename  T>
+inline T linear_to_gamma(T linear_component)
+{
+    return pow(linear_component, 1/2.2);
+}
+
+
 template<typename  T>
 void write_color(std::ostream &out, color<T> pixel_color, int samples_per_pixel){
 
@@ -18,6 +26,10 @@ void write_color(std::ostream &out, color<T> pixel_color, int samples_per_pixel)
     r *= scale;
     g *= scale;
     b *= scale;
+
+    r = linear_to_gamma<T>(r);
+    g = linear_to_gamma<T>(g);
+    b = linear_to_gamma<T>(b);
 
     static const interval<T> intensity(0.000, 0.999);
     // Write the translated [0,255] value of each color component.
